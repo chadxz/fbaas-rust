8fff680c540c904bebe01732b867f73d759b315f
Chad McElligott <chad.mcelligott@gmail.com>
2021-06-19 21:01:13 -0500

    Hello World

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use lambda_runtime::{ handler_fn, Context, Error };
+use serde_json::{ json, Value };
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let func = handler_fn(fizz_buzz);
+    lambda_runtime::run(func).await?;
+    Ok(())
+}
+
+async fn fizz_buzz(_: Value, _: Context) -> Result<Value, Error> {
+    Ok(json!({ "hello": "world" }))
+}
