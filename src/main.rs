0ed722ce7d6f04f7a8fd4b4f7edffeb7f1a38d25
Chad McElligott <chad.mcelligott@gmail.com>
2021-06-23 09:42:11 -0500

    Cleanup uses

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
-use lambda_http::lambda_runtime::Context;
-use lambda_http::handler;
-use lambda_http::{lambda_runtime, Request, IntoResponse};
-use lambda_http::lambda_runtime::Error;
-use serde_json::{ json };
+use lambda_http::lambda_runtime::{Context, Error};
+use lambda_http::{lambda_runtime, IntoResponse, Request};
+use serde_json::json;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    lambda_runtime::run(handler(fizz_buzz)).await?;
+    lambda_runtime::run(lambda_http::handler(fizz_buzz)).await?;
     Ok(())
 }
 
